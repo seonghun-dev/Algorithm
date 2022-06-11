9d67e6e5fe7f007b353964aa2aa32d3e60cedf2f
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-06-11 16:34:08 +0900

    Programmers 12910 fix

--- programmers/Level1/programmers12910.py
+++ programmers/Level1/programmers12910.py
@@ -1,3 +1,1 @@
 solution = lambda arr, divisor: answer if (answer := sorted(filter(lambda x: x > 0, list(map(lambda x: x if x % divisor == 0 else 0, arr))))) else [-1]
-
-print(solution([3, 2, 6], 10))
