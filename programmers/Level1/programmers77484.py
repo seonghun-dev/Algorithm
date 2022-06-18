fb42d515a289cf3ec3aa82ba41c613afd8cc9d83
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-06-18 22:14:07 +0900

    programmers 77484 done

--- programmers/Level1/programmers77484.py
+++ programmers/Level1/programmers77484.py
@@ -1,3 +1,6 @@
 def solution(lottos, win_nums):
-    answer = []
-    return answer
+    min, undefine = len(list(set(win_nums) - set(lottos))), lottos.count(0)
+    return [1 + min - undefine if 1 + min - undefine + 1 < 7 else 6, 1 + min if min + 1 < 7 else 6]
+
+
+print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
