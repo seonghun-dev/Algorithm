ebc1ebef87b0c55e95827016bfd9c19ace67a542
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-06-19 00:33:26 +0900

    programmers 64061 done

--- programmers/Level1/programmers64061.py
+++ programmers/Level1/programmers64061.py
@@ -1,3 +1,13 @@
 def solution(board, moves):
-    answer = 0
-    return answer
+    pickup, result = [], 0
+    for move in moves:
+        for i in range(0, len(board)):
+            if board[i][move - 1]:
+                if pickup and pickup[-1] == board[i][move - 1]:
+                    result += 2
+                    pickup.pop()
+                else:
+                    pickup.append(board[i][move - 1])
+                board[i][move - 1] = 0
+                break
+    return result
