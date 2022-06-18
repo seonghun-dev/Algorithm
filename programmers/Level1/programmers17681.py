6fe8c9159e4ab191305ea83c3a3ecc92eef29d40
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-06-18 23:30:34 +0900

    programmers 17781 done

--- programmers/Level1/programmers17681.py
+++ programmers/Level1/programmers17681.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+
 def solution(n, arr1, arr2):
-    answer = []
-    return answer
+    arr1 = [[int(k) for k in bin(r)[2:].zfill(n)] for r in arr1]
+    arr2 = [[int(k) for k in bin(r)[2:].zfill(n)] for r in arr2]
+    answer = (np.array(arr1) + np.array(arr2)).tolist()
+    result = [''.join(['#' if col > 0 else ' ' for col in row]) for row in answer]
+    return result
+
+print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
